f6c16f789f338b13e44c6397cb28a3db4f5a3aea
alexdemenezes <alexmenezes.business@outlook.com>
2023-02-22 09:39:22 -0300

    .

--- app/src/main/java/com/example/onepercentbetter/MainActivity.kt
+++ app/src/main/java/com/example/onepercentbetter/MainActivity.kt
@@ -7,29 +7,30 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.onepercentbetter.databinding.ActivityMainBinding
 import com.example.onepercentbetter.databinding.FragmentGetStartedBinding
+import com.example.onepercentbetter.databinding.FragmentLoginBinding
+import com.example.onepercentbetter.databinding.FragmentRegisterBinding
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var binding: ActivityMainBinding
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//        setupNavigation()
-//    }
-//
-//    private fun setupNavigation() {
-//        val navController = findNavController(R.id.nav_host_fragment_container)
-//        binding.bottomNavBar.setupWithNavController(navController)
-//    }
-
-    private lateinit var binding: FragmentGetStartedBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = FragmentGetStartedBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupNavigation()
     }
 
+    private fun setupNavigation() {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        binding.bottomNavBar.setupWithNavController(navController)
+    }
+
+//    private lateinit var binding: FragmentRegisterBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = FragmentRegisterBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//    }
 
 }
